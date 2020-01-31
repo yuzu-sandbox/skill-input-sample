d269eb365bd21acda1d6704dbee3112af5cd1092
yuzu <s.yuzu441@gmail.com>
2020-01-31 11:19:51 +0900

    Add production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
-module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  output: {
-    path: `${__dirname}/public/dist`,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /.tsx?$/,
-        use: "ts-loader"
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
+module.exports = (_, argv) => {
+  const isProd = argv.mode === "production"
+
+  const config = {
+    mode: isProd ? "production" : "development",
+    devtool: !isProd && "inline-source-map",
+    entry: "./src/index.tsx",
+    output: {
+      path: `${__dirname}/public/dist`,
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          use: "ts-loader"
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    }
   }
+
+  return config
 }
